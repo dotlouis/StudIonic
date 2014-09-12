2a4595e99949d50280c3afa90e229a5526272829
dotlouis <1891109+dotlouis@users.noreply.github.com>
2014-09-12 17:35:50 +0200

    Improved Onboarding
    - new layout
    - smaller slide indicator
    - learn more button
    - get started button for better call to action
    - skip button
    - new login form
    - link (void for now) to website for suscription

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,48 +12,30 @@ angular.module('studionic.controllers',[])
 	   $ionicSlideBoxDelegate.next();
 	 }
 
-	 $scope.previousSlide = function() {
-	    $ionicSlideBoxDelegate.previous();
-	  }
+	$scope.previousSlide = function() {
+		$ionicSlideBoxDelegate.previous();
+	}
 
-	$scope.showLoginPopup = function(){
-		var loginPopup = $ionicPopup.show({
-			template: '<input type="email" ng-model="user.email" placeholder="email" autofocus><br/>'+
-					  '<input type="password" ng-model="user.password" placeholder="password">',
-			title: 'Log In',
-			subTitle: 'Use your school credentials',
-			scope: $scope,
-			buttons: [
-				{ text: 'Cancel' },
-				{
-					text: '<b>Login</b>',
-					type: 'button-positive',
-					onTap: function(e) {
-						if (!$scope.user.email) {
-							alert("Please enter a valid email adress");
-							//don't allow the user to close unless he enters his credentials
-							e.preventDefault();
-						}
-						else if(!$scope.user.password){
-							alert("Please enter a password");
-							e.preventDefault();
-						}
-						else {
-							AuthFactory.signIn($scope.user.email, $scope.user.password).then(function(signedUser){
-								$state.go('app.studlife');
-							}, function(error){
-								$ionicPopup.alert({
-									title: "Login",
-									template: error.message
-								});
-							});
-							//return $scope.user;
-						}
-					}
-				},
-			]
-		});
-	};
+	$scope.lastSlide = function() {
+		$ionicSlideBoxDelegate.slide($ionicSlideBoxDelegate.slidesCount()-1);
+	}
+
+	$scope.signIn = function(){
+		if (!$scope.user.email)
+			alert("Please enter a valid email adress");
+		else if(!$scope.user.password)
+			alert("Please enter a password");
+		else {
+			AuthFactory.signIn($scope.user.email, $scope.user.password).then(function(signedUser){
+				$state.go('app.studlife');
+			}, function(error){
+				$ionicPopup.alert({
+					title: "Login",
+					template: error.message
+				});
+			});
+		}
+	}
 
 }])
 
