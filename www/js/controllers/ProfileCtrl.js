d2212a5e3a09343606e6526256ff9c30a02d34cf
dotlouis <1891109+dotlouis@users.noreply.github.com>
2014-10-23 18:33:23 +0200

    Popovers to select pictures
    - Now use popover to select profilePicture and CoverPicture in Profile
    screen (previously a tap on the said picture)
    - Fix the FAB appearing even if not admin (because of previous refactor
    of the way we know user is admin or not)

--- www/js/controllers/ProfileCtrl.js
+++ www/js/controllers/ProfileCtrl.js
@@ -1,6 +1,30 @@
 angular.module('studionic.controllers')
 
-.controller('ProfileCtrl', ['$scope','$cordovaCamera', function($scope, $cordovaCamera){
+.controller('ProfileCtrl', ['$scope','$ionicPopover','$cordovaCamera', function($scope, $ionicPopover, $cordovaCamera){
+
+    $ionicPopover.fromTemplateUrl('profilePopover.html', {
+        scope: $scope,
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
+    // $scope.openPopover = function($event) {
+    //     $scope.popover.show($event);
+    // };
+    // $scope.closePopover = function() {
+    //     $scope.popover.hide();
+    // };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+    // Execute action on hide popover
+    $scope.$on('popover.hidden', function() {
+        // Execute action
+    });
+    // Execute action on remove popover
+    $scope.$on('popover.removed', function() {
+        // Execute action
+    });
 
 
     $scope.updateProfilePicture = function(){
